adb76483a53c0a9e4c6fcd239ff15c45141f6df4
ferreiraluizga <luiz.gabriel.lcf@gmail.com>
2026-06-12 17:22:12 -0300

    feat: configurando seção de contato e projetos

---
+++ assets/js/scrollReveal.js
@@ -0,0 +1,49 @@
+ScrollReveal().reveal('#hero-section', {
+    origin: 'bottom',
+    distance: '20px',
+    duration: 1000,
+    delay: 200
+});
+
+ScrollReveal().reveal('#about-section', {
+    origin: 'bottom',
+    distance: '40px',
+    duration: 1000,
+    delay: 300
+});
+
+ScrollReveal().reveal('#tech-section', {
+    origin: 'bottom',
+    distance: '40px',
+    duration: 1000,
+    delay: 400
+});
+
+ScrollReveal().reveal('.tech-card', {
+    origin: 'bottom',
+    distance: '30px',
+    duration: 800,
+    interval: 100,
+    reset: false
+});
+
+ScrollReveal().reveal('#experience-section', {
+    origin: 'bottom',
+    distance: '40px',
+    duration: 1000,
+    delay: 400
+});
+
+ScrollReveal().reveal('#project-section', {
+    origin: 'bottom',
+    distance: '40px',
+    duration: 1000,
+    delay: 400
+});
+
+ScrollReveal().reveal('#contact-section', {
+    origin: 'bottom',
+    distance: '40px',
+    duration: 1000,
+    delay: 400
+});
